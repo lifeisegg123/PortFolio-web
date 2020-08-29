92f3d4c7c5f50cb7cc20d73b9df316eb285a6158
Lee.J.J <leejj2002@naver.com>
2020-08-29 15:42:18 +0000

    autoship done maybe?

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -14,9 +14,7 @@ const deltaRB = {
 };
 const autonomousShip = {
   name: "autonomousShip",
-  imgs: [
-    "/PortFolio-web/assets/img/autonomous_ship1.jpg",
-  ],
+  imgs: ["/PortFolio-web/assets/img/autonomous_ship1.jpg"],
   vids: ["/PortFolio-web/assets/video/autonomous_ship.mp4"],
   texts: "얼굴을 인식하여 엣지를 추출한 후 영상을 탐색하여 그림을 그리는 로봇",
   link: "https://github.com/lifeisegg123/delta_robot",
@@ -31,6 +29,7 @@ function handleModalClick() {
   const name = event.target.alt;
   const title = document.createElement("h2");
   const imgDiv = document.createElement("div");
+  const videoDiv = document.createElement("div");
   const texts = document.createElement("p");
   const aTag = document.createElement("a");
 
@@ -46,8 +45,14 @@ function handleModalClick() {
   if (targetModal) {
     title.innerText = targetModal.name;
     title.className = "modal__title";
+    modalDiv.appendChild(title);
     texts.innerText = targetModal.texts;
     texts.className = "modal__texts";
+    modalDiv.appendChild(texts);
+    aTag.innerText = "Source Link";
+    aTag.href = targetModal.link;
+    aTag.className = "modal__link";
+    modalDiv.appendChild(aTag);
     if (targetModal.imgs) {
       targetModal.imgs.forEach((value) => {
         const img = document.createElement("img");
@@ -55,15 +60,18 @@ function handleModalClick() {
         img.className = "modal__img";
         imgDiv.appendChild(img);
       });
+      imgDiv.className = "modal__imgdiv";
+      modalDiv.appendChild(imgDiv);
     }
-    imgDiv.className = "modal__imgdiv";
-    aTag.innerText = "Source Link";
-    aTag.href = targetModal.link;
-    aTag.className = "modal__link";
-    modalDiv.appendChild(title);
-    modalDiv.appendChild(texts);
-    modalDiv.appendChild(aTag);
-    modalDiv.appendChild(imgDiv);
+    if (targetModal.vids) {
+      targetModal.vids.forEach((value) => {
+        const vid = document.createElement("video");
+        vid.src = `${value}`;
+        vid.className = "modal__vid";
+        videoDiv.appendChild(vid);
+      });
+      modalDiv.appendChild(videoDiv);
+    }
     modalDiv.className = "modal";
     modalSection.appendChild(modalDiv);
   }
